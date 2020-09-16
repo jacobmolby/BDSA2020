d6cf275d4983357688ec0c44121e2382f82825b8
Rasmus Lystrøm <rasmusl@microsoft.com>
2020-09-16 08:55:07 +0200

    a

--- Lecture04/Lecture04/Program.cs
+++ Lecture04/Lecture04/Program.cs
@@ -1,4 +1,6 @@
-﻿// using Microsoft.Extensions.Configuration;
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace Lecture04
 {
@@ -6,11 +8,36 @@ namespace Lecture04
     {
         static void Main(string[] args)
         {
-            // var configuration = new ConfigurationBuilder()
-            //     .AddUserSecrets(typeof(Program).Assembly)
-            //     .Build();
+            var configuration = new ConfigurationBuilder()
+                .AddUserSecrets(typeof(Program).Assembly)
+                .Build();
 
-            // var connectionString = configuration.GetConnectionString("ConnectionString");
+            var connectionString = configuration.GetConnectionString("ConnectionString");
+
+            // Console.WriteLine(connectionString.Substring(0, 10));
+
+            // using var connection = new SqlConnection(connectionString);
+
+            // connection.Open();
+
+            // Console.Write("Input search string: ");
+
+            // var searchString = Console.ReadLine();
+
+            // var cmdText = $"SELECT * FROM Characters WHERE Name LIKE '%' + @SearchString + '%'";
+
+            // using var command = new SqlCommand(cmdText, connection);
+            // command.Parameters.AddWithValue("@SearchString", searchString);
+
+            // using var reader = command.ExecuteReader();
+
+            // while (reader.Read())
+            // {
+            //     Console.WriteLine(reader["Name"]);
+            // }
+
+            RawSqlDemo.Run(connectionString);
+
         }
     }
 }
--- Lecture04/Lecture04/RawSqlCharacterRepository.cs
+++ Lecture04/Lecture04/RawSqlCharacterRepository.cs
@@ -1,160 +1,160 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Data;
-// using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 
-// namespace Lecture04
-// {
-//     public class RawSqlCharacterRepository : IDisposable
-//     {
-//         private readonly SqlConnection _connection;
+namespace Lecture04
+{
+    public class RawSqlCharacterRepository : IDisposable
+    {
+        private readonly SqlConnection _connection;
 
-//         public RawSqlCharacterRepository(string connectionString)
-//         {
-//             _connection = new SqlConnection(connectionString);
-//         }
+        public RawSqlCharacterRepository(string connectionString)
+        {
+            _connection = new SqlConnection(connectionString);
+        }
 
-//         public int Create(CharacterDTO character)
-//         {
-//             var cmdText = @"INSERT Character (Name, Species, Planet, ActorId)
-//                             VALUES (@Name, @Species, @Planet, @ActorId);
-//                             SELECT SCOPE_IDENTITY()";
+        public int Create(CharacterDTO character)
+        {
+            var cmdText = @"INSERT Character (Name, Species, Planet, ActorId)
+                            VALUES (@Name, @Species, @Planet, @ActorId);
+                            SELECT SCOPE_IDENTITY()";
 
-//             using var command = new SqlCommand(cmdText, _connection);
+            using var command = new SqlCommand(cmdText, _connection);
 
-//             command.Parameters.AddWithValue("@Name", character.Name);
-//             command.Parameters.AddWithValue("@Species", character.Species);
-//             command.Parameters.AddWithValue("@Planet", character.Planet);
-//             command.Parameters.AddWithValue("@ActorId", character.ActorId);
+            command.Parameters.AddWithValue("@Name", character.Name);
+            command.Parameters.AddWithValue("@Species", character.Species);
+            command.Parameters.AddWithValue("@Planet", character.Planet);
+            command.Parameters.AddWithValue("@ActorId", character.ActorId);
 
-//             OpenConnection();
+            OpenConnection();
 
-//             var id = command.ExecuteScalar();
+            var id = command.ExecuteScalar();
 
-//             CloseConnection();
+            CloseConnection();
 
-//             return (int)id;
-//         }
+            return (int)id;
+        }
 
-//         public CharacterDTO Read(string name)
-//         {
-//             var cmdText = @"SELECT c.Id, c.Name, c.Species, c.Planet, c.ActorId, a.Name AS ActorName
-//                             FROM Characters AS c
-//                             LEFT JOIN Actors AS a ON c.ActorId = a.Id
-//                             WHERE c.Name = @Name";
+        public CharacterDTO Read(string name)
+        {
+            var cmdText = @"SELECT c.Id, c.Name, c.Species, c.Planet, c.ActorId, a.Name AS ActorName
+                            FROM Characters AS c
+                            LEFT JOIN Actors AS a ON c.ActorId = a.Id
+                            WHERE c.Name = @Name";
 
-//             using var command = new SqlCommand(cmdText, _connection);
+            using var command = new SqlCommand(cmdText, _connection);
 
-//             command.Parameters.AddWithValue("@Name", name);
+            command.Parameters.AddWithValue("@Name", name);
 
-//             OpenConnection();
+            OpenConnection();
 
-//             using var reader = command.ExecuteReader();
+            using var reader = command.ExecuteReader();
 
-//             var character = reader.Read()
-//                 ? new CharacterDTO
-//                 {
-//                     Id = reader.GetInt32("Id"),
-//                     Name = reader.GetString("Name"),
-//                     Species = reader.GetString("Species"),
-//                     Planet = reader.GetString("Planet"),
-//                     ActorId = reader.GetInt32("ActorId"),
-//                     ActorName = reader.GetString("ActorName")
-//                 }
-//                 : null;
+            var character = reader.Read()
+                ? new CharacterDTO
+                {
+                    Id = reader.GetInt32("Id"),
+                    Name = reader.GetString("Name"),
+                    Species = reader.GetString("Species"),
+                    Planet = reader.GetString("Planet"),
+                    ActorId = reader.GetInt32("ActorId"),
+                    ActorName = reader.GetString("ActorName")
+                }
+                : null;
 
-//             CloseConnection();
+            CloseConnection();
 
-//             return character;
-//         }
+            return character;
+        }
 
-//         public IEnumerable<CharacterDTO> Read()
-//         {
-//             var cmdText = @"SELECT c.Id, c.Name, c.Species, c.Planet, c.ActorId, a.Name AS ActorName
-//                             FROM Characters AS c
-//                             LEFT JOIN Actors AS a ON c.ActorId = a.Id
-//                             ORDER BY c.Name";
+        public IEnumerable<CharacterDTO> Read()
+        {
+            var cmdText = @"SELECT c.Id, c.Name, c.Species, c.Planet, c.ActorId, a.Name AS ActorName
+                            FROM Characters AS c
+                            LEFT JOIN Actors AS a ON c.ActorId = a.Id
+                            ORDER BY c.Name";
 
-//             using var command = new SqlCommand(cmdText, _connection);
+            using var command = new SqlCommand(cmdText, _connection);
 
-//             OpenConnection();
+            OpenConnection();
 
-//             using var reader = command.ExecuteReader();
+            using var reader = command.ExecuteReader();
 
-//             while (reader.Read())
-//             {
-//                 yield return new CharacterDTO
-//                 {
-//                     Id = reader.GetInt32("Id"),
-//                     Name = reader.GetString("Name"),
-//                     Species = reader.GetString("Species"),
-//                     Planet = reader.GetString("Planet"),
-//                     ActorId = reader.GetInt32("ActorId"),
-//                     ActorName = reader.GetString("ActorName")
-//                 };
-//             }
+            while (reader.Read())
+            {
+                yield return new CharacterDTO
+                {
+                    Id = reader.GetInt32("Id"),
+                    Name = reader.GetString("Name"),
+                    Species = reader.GetString("Species"),
+                    Planet = reader.GetString("Planet"),
+                    ActorId = reader.GetInt32("ActorId"),
+                    ActorName = reader.GetString("ActorName")
+                };
+            }
 
-//             CloseConnection();
-//         }
+            CloseConnection();
+        }
 
-//         public void Update(CharacterDTO character)
-//         {
-//             var cmdText = @"UPDATE Characters SET
-//                             Name = @Name,
-//                             Species = @Species,
-//                             Planet = @Planet,
-//                             ActorId = @ActorId
-//                             WHERE Id = @Id";
+        public void Update(CharacterDTO character)
+        {
+            var cmdText = @"UPDATE Characters SET
+                            Name = @Name,
+                            Species = @Species,
+                            Planet = @Planet,
+                            ActorId = @ActorId
+                            WHERE Id = @Id";
 
-//             using var command = new SqlCommand(cmdText, _connection);
+            using var command = new SqlCommand(cmdText, _connection);
 
-//             command.Parameters.AddWithValue("@Id", character.Id);
-//             command.Parameters.AddWithValue("@Name", character.Name);
-//             command.Parameters.AddWithValue("@Species", character.Species);
-//             command.Parameters.AddWithValue("@Planet", character.Planet);
-//             command.Parameters.AddWithValue("@ActorId", character.ActorId);
+            command.Parameters.AddWithValue("@Id", character.Id);
+            command.Parameters.AddWithValue("@Name", character.Name);
+            command.Parameters.AddWithValue("@Species", character.Species);
+            command.Parameters.AddWithValue("@Planet", character.Planet);
+            command.Parameters.AddWithValue("@ActorId", character.ActorId);
 
-//             OpenConnection();
+            OpenConnection();
 
-//             command.ExecuteNonQuery();
+            command.ExecuteNonQuery();
 
-//             CloseConnection();
-//         }
+            CloseConnection();
+        }
 
-//         public void Delete(int characterId)
-//         {
-//             var cmdText = @"DELETE Characters WHERE Id = @Id";
+        public void Delete(int characterId)
+        {
+            var cmdText = @"DELETE Characters WHERE Id = @Id";
 
-//             using var command = new SqlCommand(cmdText, _connection);
+            using var command = new SqlCommand(cmdText, _connection);
 
-//             command.Parameters.AddWithValue("@Id", characterId);
+            command.Parameters.AddWithValue("@Id", characterId);
 
-//             OpenConnection();
+            OpenConnection();
 
-//             command.ExecuteNonQuery();
+            command.ExecuteNonQuery();
 
-//             CloseConnection();
-//         }
+            CloseConnection();
+        }
 
-//         private void OpenConnection()
-//         {
-//             if (_connection.State == ConnectionState.Closed)
-//             {
-//                 _connection.Open();
-//             }
-//         }
+        private void OpenConnection()
+        {
+            if (_connection.State == ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }
 
-//         private void CloseConnection()
-//         {
-//             if (_connection.State == ConnectionState.Open)
-//             {
-//                 _connection.Close();
-//             }
-//         }
+        private void CloseConnection()
+        {
+            if (_connection.State == ConnectionState.Open)
+            {
+                _connection.Close();
+            }
+        }
 
-//         public void Dispose()
-//         {
-//             _connection.Dispose();
-//         }
-//     }
-// }
+        public void Dispose()
+        {
+            _connection.Dispose();
+        }
+    }
+}
